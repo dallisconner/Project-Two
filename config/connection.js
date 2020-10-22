d068df0ff602afd26a4af01b472f5a5c0e28490f
Victoria Smith <victoriasmithh428@gmail.com>
2020-10-21 21:16:50 -0400

    created sql database name

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,7 @@ let Sequelize = require('sequelize')
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
 
-//the first empty string is the database name
-let sequelize = new Sequelize('', 'root', '', {
+let sequelize = new Sequelize('spooky_db', 'root', '', {
   host: 'localhost',
   port: 3306,
   dialect: 'mysql',
