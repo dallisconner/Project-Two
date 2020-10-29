15452f7f9fed7dda2c62a3bd630f7b33edeb6e7c
Victoria Smith <victoriasmithh428@gmail.com>
2020-10-28 21:08:21 -0400

    working on favorites routes

---
+++ controllers/favorites-route.js
@@ -0,0 +1,33 @@
+const { default: Axios } = require('axios')
+let app = require('express')
+const db = require('../models')
+const favorites = require('../models/favorites')
+
+let movieURL = 'https://api.themoviedb.org/3/movie/550?api_key=`{apiKey}`'
+app.get('/favorites', (req, res) => {
+  let movie = axios
+    .get(
+      'https://api.themoviedb.org/3/discover/movie?api_key=`{apiKey}`&with_genres=53,27,10751,35,18'
+    )
+    .then((response) => {
+      const response = sqlRes.map(async (obj) => {
+        const movie = await getMovieById(obj.movie_id)
+        return { ...obj, movie }
+      }, res.json(response))
+    })
+})
+
+app.post('/favorites', (req, res) => {
+  axios.post(movieURL)
+  req.body(
+    {
+      username: 'foo',
+      snack: 'Swedish fish',
+      drink: 'Manhattan',
+      movieId: 'abc124',
+    },
+    favorites.save(req.body)
+  )
+})
+
+module.exports(app)
--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,22 @@ DROP DATABASE IF EXISTS spooky_db;
 CREATE DATABASE  spooky_db;
 
 USE spooky_db;
+
+CREATE TABLE favorites
+(
+    id
+        AUTO_INCREMENT
+    NUMBER,
+username VARCHAR
+    (100),
+drink
+VARCHAR
+    (100),
+snack
+VARCHAR
+    (100),
+movie_id
+VARCHAR
+    (100)
+)
+
