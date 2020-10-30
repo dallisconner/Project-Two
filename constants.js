d34f30ee89cb5ce5e1eda8443ae8ebf0619e7698
Victoria Smith <victoriasmithh428@gmail.com>
2020-10-29 21:43:06 -0400

    set up results route

---
+++ constants.js
@@ -0,0 +1,4 @@
+module.exports.apiKey = 'c4927d46648db6c2fb7393b2734c0eeb'
+module.exports.baseURL = 'https://api.themoviedb.org/3'
+module.exports.moviePath = '/movie'
+module.exports.discoverPath = '/discover/movie'
